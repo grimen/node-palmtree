0a5784fa0412e32bafd7041739f80a6564f0126e
Jonas Grimfelt <grimen@gmail.com>
2019-03-07 03:55:43 -0500

    doc: basic usage example - `examples/basic.js`

--- examples/basic.js
+++ examples/basic.js
@@ -1,9 +1,9 @@
 
 async function main () {
 
-    // =========================================
-    //       EXAMPLE
-    // --------------------------------------
+    /* =======================================================
+        IMPORTS
+    ----------------------------------------------------- */
 
     const palmtree = require('../')
 
@@ -11,45 +11,85 @@ async function main () {
     const color = require('chalk')
     const fs = require('fs')
 
-    // ---------------------------------------------------
-    //   EXAMPLE: plain colorized output
-    // ------------------------------------------------
+    /* =======================================================
+        EXAMPLE: log plain colorized output
+    ----------------------------------------------------- */
 
-    await palmtree.log('~')
+    console.log(`[log/plain]: plain colorized output`)
 
-    // ---------------------------------------------------
-    //   EXAMPLE: detailed custom colorized output
-    // ------------------------------------------------
+    await palmtree.log('./test/__fixtures__/foo')
 
-    await palmtree.log('~', {
-        meta: async (item) => {
-            try {
-                const stat = await util.promisify(fs.stat)(item.resolvedPath)
+    /* =======================================================
+        EXAMPLE: log detailed custom colorized output
+    ----------------------------------------------------- */
 
-                let data
+    console.log(`[log/detailed]: detailed custom colorized output`)
 
-                if (item.isFile) {
-                    data = await util.promisify(fs.readFile)(item.resolvedPath, 'utf8')
-                }
+    const meta = async (item) => {
+        try {
+            const stat = await util.promisify(fs.stat)(item.resolvedPath)
 
-                return [
-                    color.gray('- '),
-                    [
-                        color.bgMagenta(` ${stat.size} bytes `),
-                        data && JSON.stringify({data}),
-                        color.cyan(`${stat.ctime}`),
-                    ].join(color.gray(' ')),
-                ].join('')
+            let data
 
-            } catch (error) {
-                return color.yellow(`(!) could not read/resolve`)
+            if (item.isFile) {
+                data = await util.promisify(fs.readFile)(item.resolvedPath, 'utf8')
             }
-        }
-    })
 
-    // ---------------------------------------------------
-    //   EXAMPLE: detailed custom colorized result
-    // ------------------------------------------------
+            return [
+                color.gray('- '),
+                [
+                    color.bgMagenta(` ${stat.size} bytes `),
+                    data && JSON.stringify({data}),
+                    color.cyan(`${stat.ctime}`),
+                ].join(color.gray(' ')),
+            ].join('')
+
+        } catch (error) {
+            return color.yellow(`(!) could not read/resolve`)
+        }
+    }
+
+    await palmtree.log('./test/__fixtures__/foo', {meta})
+
+    /* =======================================================
+        EXAMPLE: inspect plain colorized output
+    ----------------------------------------------------- */
+
+    console.log(`[inspect/plain]: plain colorized output`)
+
+    let inspection = await palmtree.inspect('./test/__fixtures__/foo')
+
+    console.log('[inspect/plain]:', inspection)
+
+    /* =======================================================
+        EXAMPLE: inspect detailed custom colorized output
+    ----------------------------------------------------- */
+
+    console.log(`[inspect/detailed]: detailed custom colorized output`)
+
+    inspection = await palmtree.inspect('./test/__fixtures__/foo', {meta})
+
+    console.log('[inspect/detailed]:', inspection)
+
+    /* =======================================================
+        EXAMPLE: get plain object
+    ----------------------------------------------------- */
+
+    console.log(`[get/plain]: plain colorized output`)
+
+    let tree = await palmtree.get('./test/__fixtures__/foo')
+
+    console.log('[get/plain]:', tree)
+
+    /* =======================================================
+        EXAMPLE: get detailed object
+    ----------------------------------------------------- */
+
+    console.log(`[get/detailed]: detailed custom colorized output`)
+
+    tree = await palmtree.get('./test/__fixtures__/foo', {meta})
+
+    console.log('[get/detailed]:', tree)
 }
 
 main()
